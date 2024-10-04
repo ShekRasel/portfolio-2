ed0ad2130a23e372df1e3e8308e8df5cb2609baa
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-10-04 17:34:03 +0600

    image path mod for testing

--- src/components/GetInform.jsx
+++ src/components/GetInform.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function GetInform() {
   return (
     <div className='pt-24 pb-24 bg-gray-50 z-10' id='getinfo'>
-        <div className="bg-[url('image/1.png')] h-96 bg-contain bg-center border-none">
+        <div className="bg-[url('/image/1.png')] h-96 bg-contain bg-center border-none">
             <div className='w-full h-full bg-indigo-700 opacity-75 flex justify-center'>
                 <div className='   h-full pb-24 pt-24 flex flex-col items-center justify-center px-6 '>
                     <h1 className='text-2xl md:text-4xl text-center font-extrabold text-white'>Have any project on mind?</h1>
