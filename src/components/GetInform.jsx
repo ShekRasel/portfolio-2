715907397c94318c48b8aadf61790dec74b3e5b3
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-10-05 14:53:56 +0600

    text edit

--- src/components/GetInform.jsx
+++ src/components/GetInform.jsx
@@ -7,7 +7,7 @@ function GetInform() {
             <div className='w-full h-full bg-indigo-700 opacity-75 flex justify-center'>
                 <div className='   h-full pb-24 pt-24 flex flex-col items-center justify-center px-6 '>
                     <h1 className='text-2xl md:text-4xl text-center font-extrabold text-white'>Have any project on mind?</h1>
-                    <p className='text-white mt-2 text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti explicabo deserunt recusandae atque eaque neque.</p>
+                    <p className='text-white mt-2 text-center'>If you want to get services from me then you can hire me, and also download my cv from here for any needed.</p>
                     <div className='flex flex-col md:flex-row gap-6 mt-5 '>
                         <button className='bg-white px-10 py-3 rounded-full text-indigo-600  hover:bg-purple-600 hover:text-white transition-all ease-in-out duration-500'>Download CV</button>
                         <button className=' px-14 py-3 rounded-full text-white font-semibold border hover:bg-indigo-400 transition-all ease-in-out duration-500'>Hire Me</button>
