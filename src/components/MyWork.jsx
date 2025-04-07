7254028a225e9dc3aaf1542261b7518611ab70b7
Shek Rasel <your-email@example.com>
2025-04-07 22:50:50 +0600

    motion add

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -68,8 +68,6 @@ function MyWork() {
       name: "YouTube Clone, html/css",
       projectLink: "https://youtube-clone-eight-sable.vercel.app/",
     },
-    
-    
   ];
 
   return (
@@ -103,8 +101,8 @@ function MyWork() {
         {projects.slice(0, visible).map((project, index) => (
           <motion.div
             initial={{ scale: 0, opacity: 0 }}
-            whileInView={{scale:1, opacity:1}}
-            viewport={{once:true}}
+            whileInView={{ scale: 1, opacity: 1 }}
+            viewport={{ once: true }}
             transition={{ delay: index * 0.2, ease: "easeInOut" }}
             className="h-64 shadow-lg bg-cover mt-10 rounded-lg hover:scale-105 ease-in-out transition-all duration-700"
             onMouseEnter={() => showElement(index)}
@@ -112,9 +110,9 @@ function MyWork() {
             key={index}
             style={{ backgroundImage: `url(${project.image})` }}
           >
-            <div className="w-full h-full hover:bg-indigo-900 opacity-75 transition-all ease-in-out duration-500 flex justify-center shadow-xl rounded-lg">
+            <div className="w-full h-full hover:bg-[#121212] opacity-75 transition-all ease-in-out duration-500 flex justify-center shadow-xl rounded-lg">
               {state === index && (
-                <div className="flex flex-col items-center justify-center overflow-hidden">
+                <div className="flex flex-col items-center justify-center overflow-hidden  w-full ">
                   {/* Text Animation */}
                   <div>
                     <h1
@@ -125,9 +123,12 @@ function MyWork() {
                   </div>
 
                   {/* Icons Animation */}
-                  <div className="flex gap-6 border-t pt-6 mt-6">
+                  <div className="flex gap-6 border-t pt-6 mt-6 relative">
                     {/* Left Icon Animation */}
-                    <span
+                    <motion.span
+                      initial={{ x: -34, opacity: 0 }}
+                      whileInView={{ x: 0, opacity: 1 }}
+                      transition={{ delay: 0.1 }}
                       className={`border text-xl rounded-full p-4 text-white hover:bg-white hover:text-indigo-700 transition-all ease-in-out duration-500 transform ${
                         state === index
                           ? "translate-x-0 opacity-100"
@@ -135,10 +136,26 @@ function MyWork() {
                       }`}
                     >
                       <BsPlusLg />
-                    </span>
+
+                      {/* Tooltip */}
+                      <motion.div
+                        initial={{ opacity: 0 }}
+                        whileHover={{ opacity: 1 }}
+                        transition={{ duration: 0.3 }}
+                        className="absolute bottom-1 left-5  transform -translate-x-1/2  text-white text-sm py-16 rounded-lg"
+                      >
+                        <button className="bg-indigo-600 border rounded-md px-4 py-2 shadow-md">
+
+                        Description
+                        </button>
+                      </motion.div>
+                    </motion.span>
 
                     {/* Right Icon Animation */}
-                    <span
+                    <motion.span
+                      initial={{ x: 34, opacity: 0 }}
+                      whileInView={{ x: 0, opacity: 1 }}
+                      transition={{ delay: 0.1 }}
                       className={`border text-xl rounded-full p-4 text-white hover:bg-white hover:text-indigo-700 transition-all ease-in-out duration-500 transform ${
                         state === index
                           ? "translate-x-0 opacity-100"
@@ -152,7 +169,7 @@ function MyWork() {
                       >
                         <HiLink />
                       </a>
-                    </span>
+                    </motion.span>
                   </div>
                 </div>
               )}
@@ -162,15 +179,15 @@ function MyWork() {
       </div>
       <div className="w-full flex justify-center">
         {projects.length > visible && (
-            <motion.button
-            initial={{y:20}}
-            whileInView={{y:0}}
-            viewport={{once:true}}
-              onClick={() => setVisible(projects.length)}
-              className="bg-indigo-700 rounded-full px-12 mt-14 py-4 font-semibold text-white hover:bg-white hover:text-indigo-700 hover:border-indigo-600 hover:outline hover:outline-1 shadow-slate-400 shadow-md hover:shadow-indigo-700 transition-all ease-in-out duration-500"
-            >
-              More Works ...
-            </motion.button>
+          <motion.button
+            initial={{ y: 20 }}
+            whileInView={{ y: 0 }}
+            viewport={{ once: true }}
+            onClick={() => setVisible(projects.length)}
+            className="bg-indigo-700 rounded-full px-12 mt-14 py-4 font-semibold text-white hover:bg-white hover:text-indigo-700 hover:border-indigo-600 hover:outline hover:outline-1 shadow-slate-400 shadow-md hover:shadow-indigo-700 transition-all ease-in-out duration-500"
+          >
+            More Works ...
+          </motion.button>
         )}
       </div>
     </div>
