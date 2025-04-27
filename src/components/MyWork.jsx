95cdec5facf99c4a5bfe49052dfe4651f10dc0bd
Shek Rasel <your-email@example.com>
2025-04-27 22:32:01 +0600

    new project add

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { BsPlusLg } from "react-icons/bs";
 import { HiLink } from "react-icons/hi2";
 import { motion, useInView } from "motion/react";
+import {projects} from '../Projects/AllProjects'
 function MyWork() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
@@ -16,59 +17,6 @@ function MyWork() {
     setState(null);
   };
 
-  const projects = [
-    {
-      image: "image/ecommere.png",
-      name: "Synzo Ecommerce site",
-      projectLink: "https://ecommerce-site-two-drab.vercel.app/",
-    },
-
-    {
-      image: "image/expense.png",
-      name: "Expense Tracker",
-      projectLink: "https://expense-tracker-mu-puce.vercel.app/",
-    },
-    {
-      image: "image/cryptoland.png",
-      name: "Cryptoland",
-      projectLink: "https://cryptoland-chi.vercel.app/",
-    },
-
-    {
-      image: "image/project1.png",
-      name: "NFT Market Place",
-      projectLink: "https://nft-market-place-chi-eight.vercel.app/",
-    },
-    {
-      image: "image/hotel.png",
-      name: "Hotel management",
-      projectLink: "https://da-hotel-website.vercel.app/",
-    },
-
-    {
-      image: "image/project2.png",
-      name: "Event Management System",
-      projectLink: "https://frontend-eight-steel-19.vercel.app/",
-    },
-
-    {
-      image: "image/project3.png",
-      name: "Education Landing page",
-      projectLink: "https://education-site-henna.vercel.app/",
-    },
-
-    {
-      image: "image/weather_og.png",
-      name: "Weather App",
-      projectLink: "https://react-weather-app-silk-five.vercel.app/",
-    },
-
-    {
-      image: "image/youtube.png",
-      name: "YouTube Clone, html/css",
-      projectLink: "https://youtube-clone-eight-sable.vercel.app/",
-    },
-  ];
 
   return (
     <div
